483da93c71bec1f0812c00531a992f21e85acc27
hightoon <haitong.chen@gmail.com>
2018-07-14 22:01:20 +0800

    add new cases for Network.distance

--- socialnetwork/tests/test_main.py
+++ socialnetwork/tests/test_main.py
@@ -1,4 +1,9 @@
 #-*- coding: utf-8 -*-
+
+""" 
+unit test for main.py
+"""
+
 import sys
 sys.path.insert(0, '..')
 
@@ -8,12 +13,17 @@ from solution import main
 
 class TestMian(unittest.TestCase):
     def test_name_to_id(self):
-        people = main.read_people('../data/test_data_1.txt')
+        people, _ = main.read_people('../data/test_data_1.txt')
         assert(people['HORACE_LAMPSHIRE'] == 0)
         assert(people['ELLSWORTH_BRILLA'] == 4)
 
+    def test_friend_pair(self):
+        _, pairs = main.read_people('../data/test_data_1.txt')
+        assert(len(pairs) == 4)
+        assert(pairs[3] == ['HORACE_LAMPSHIRE', 'ELLSWORTH_BRILLA'])
+
     def test_count_num_of_people(self):
-        assert(main.count_num_of_people(main.read_people('../data/test_data_1.txt')) == 5)
+        assert(main.count_num_of_people(main.read_people('../data/test_data_1.txt')[0]) == 5)
 
 
 if __name__ == '__main__':
--- socialnetwork/tests/test_network.py
+++ socialnetwork/tests/test_network.py
@@ -9,7 +9,7 @@ sys.path.insert(0, '..')
 
 
 import unittest
-from solution import network
+from solution import network, main
 
 
 class TestNetwork(unittest.TestCase):
@@ -22,6 +22,36 @@ class TestNetwork(unittest.TestCase):
     def test_queue_dequeue_exception(self):
         q = network.Queue()
         self.assertRaises(IndexError, q.dequeue)
+
+    def test_network_distance_success_dist1(self):
+        map_, pairs = main.read_people('../data/test_data_1.txt')
+        net = network.Network(pairs, map_)
+        assert(net.distance('HORACE_LAMPSHIRE', 'JOAN_SCHWIMMER') == 1)
+
+    def test_network_distance_success_dist2(self):
+        map_, pairs = main.read_people('../data/SocialNetwork.txt')
+        net = network.Network(pairs, map_)
+        assert(net.distance('ARON_OHAIR', 'DEMARCUS_HAMILL') == 2)
+
+    def test_network_distance_success_dist3(self):
+        map_, pairs = main.read_people('../data/SocialNetwork.txt')
+        net = network.Network(pairs, map_)
+        assert(net.distance('CHARLIE_PAYTES', 'HECTOR_MICHENER') == 3)
+
+    def test_network_distance_success_dist4(self):
+        map_, pairs = main.read_people('../data/SocialNetwork.txt')
+        net = network.Network(pairs, map_)
+        assert(net.distance('CHARLIE_PAYTES', 'CHRISTOPER_BRAKE') == 4)
+
+    def test_network_distance_success_dist5(self):
+        map_, pairs = main.read_people('../data/SocialNetwork.txt')
+        net = network.Network(pairs, map_)
+        assert(net.distance('CHARLIE_PAYTES', 'SCOTTY_NEMAN') == 5)
+
+    def test_network_distance_success_dist6(self):
+        map_, pairs = main.read_people('../data/SocialNetwork.txt')
+        net = network.Network(pairs, map_)
+        assert(net.distance('CHARLIE_PAYTES', 'ART_DELRE') == 5)
 
 
 if __name__ == '__main__':
